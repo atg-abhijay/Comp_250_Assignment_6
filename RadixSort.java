748114bb29cab20c912731a0fb4a2e416b12976b
atg-abhijay <atg.abhijay@gmail.com>
2017-04-07 23:55:23 -0400

    Fixed it. On line 51 it should have been m < 10 instead of m < array.length since m is denoting the number of buckets and that is always 10. It is independent of the length of the array

--- RadixSort.java
+++ RadixSort.java
@@ -1,9 +1,15 @@
 import java.util.*;
 public class RadixSort {
     public static void main(String[] args) {
-        int[] array = {23,1,5,102,20,56,7,15};
-        int[] answer = radixSort(array);
+        int[] array = new int[8];
+        //int[] array = {7,2,9,8};
+        for(int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 50);
+            System.out.print(array[i] + " ");
+        }
 
+        System.out.println("\n");
+        int[] answer = radixSort(array);
         for(int k = 0; k < answer.length; k++) {
             System.out.print(answer[k] + " ");
         }
@@ -20,8 +26,9 @@ public class RadixSort {
             copied[i] = num;                                        // assign + index
         }                                                       // lines 16-20:             = 7n
 
-        
+        //System.out.println("Max: " + max);
         int maxNumDigits = (int) Math.log10(max) + 1;           // cast + arith + arith             = 3
+        //System.out.println("Max number of digits: " + maxNumDigits);
         LinkedList<LinkedList<Integer>> buckets = new LinkedList<LinkedList<Integer>>(); // assign  = 1
         for(int p = 0; p < 10; p++) {                           // 1 + 11 + 1                       = 13
             buckets.add(new LinkedList<Integer>());             // 10                               = 10
@@ -31,7 +38,9 @@ public class RadixSort {
         for(int j = 1; j <= maxNumDigits; j++) {                // 1 + (maxNumDigits+1) + maxNumDigits      = p 
             for(int k = 0; k < array.length; k++) {                 // 1 + (n+1) + n                        = p * (2n+2)
                 int number = copied[k];                                             // index + assign       
+                //System.out.println("Number: " + number);
                 int digit = (int) ((number % Math.pow(10, j)) / Math.pow(10,j-1));  // cast + 5 * arith    
+                //System.out.println("Digit: " + digit);
                 buckets.get(digit).add(number);                                     // index + add          
             }                                                                       // lines 33-35:         = (pn) * 10
 
@@ -39,12 +48,13 @@ public class RadixSort {
             int numAt = 0;                                          // assign                               = p
 
             search:
-            for(int m = 0; m < array.length; m++) {                 // 1 + (n+1) + n                        = p * (2n+2)
-                while(!buckets.get(m).isEmpty()) {                                  // index + method       = pn * 2
-                    partiallySorted[numAt] = buckets.get(m).pollFirst();                // index + poll + index + assign   = pn * 2 * 4
-                    numAt++;                                                            // arith                           = pn * 2
+            for(int m = 0; m < 10; m++) {                           // 1 + 11 + 10                          = p * (22)
+                while(!buckets.get(m).isEmpty()) {                                  // index + method       = 10p * 2
+                    partiallySorted[numAt] = buckets.get(m).pollFirst();                // index + poll + index + assign   = 10p * 2 * 4
+                    //System.out.println("Added: " + partiallySorted[numAt]);
+                    numAt++;                                                            // arith                           = 10p * 2
                 }
-                if(numAt == array.length) {                                         // getlength + cond + comp = pn * 3
+                if(numAt == array.length) {                                         // getlength + cond + comp = 10p * 3
                     break search;                                                       // 1                   = 1
                 }
             }
